e6aeb7d65491eb49613f4cf446925b2eebf99703
kristofer <kristofer@kravrr.com>
2022-04-07 15:33:14 +0800

    Install axios and fix package compatibility issue

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,31 @@
 console.log('Test Webpack');
+import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import VueApexCharts from 'vue-apexcharts';
+import VueConfirmDialog from 'vue-confirm-dialog';
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate/dist/vee-validate.full';
+import IpAddressIndex from './pages/ip_address/IpAddressIndex';
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.use(VueApexCharts);
+Vue.use(VueConfirmDialog);
+Vue.component('ValidationProvider', ValidationProvider);
+Vue.component('ValidationObserver', ValidationObserver);
+Vue.component('vue-confirm-dialog', VueConfirmDialog.default);
+Vue.component('extend', extend);
+Vue.component('apexchart', VueApexCharts);
+
+// Vue init
+window.IpAddressManagementApp = new Vue({
+    el: '#app',
+    components: {
+        IpAddressIndex,
+    },
+    data() {
+        return {
+            showLoadingOverlay: false
+        }
+    }
+});
+
